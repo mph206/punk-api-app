cab0bd0ab479b5ca9aa587b1c08ae5958ce6b7db
Martin Harrison <mpharrison206@gmail.com>
2020-07-04 19:21:23 +0100

    working filters

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,50 @@
 import React, { Component } from 'react';
-import './App.css';
+import styles from './App.module.scss';
+import beers from './data/beers';
+import Main from './component/Main';
+import Navbar from './component/Navbar';
 
 export default class App extends Component {
+
+  state = {
+    searchTerm: '',
+    highAlcoholOnly: false,
+    classicRangeOnly: false,
+    highAcidityOnly: false,
+  }
+
+  handleSearch = (event) => {
+    this.setState({
+      searchTerm: event.target.value,
+    })
+  }
+
+  filterClassic = () => {
+    this.setState({
+      classicRangeOnly: !this.state.classicRangeOnly,
+    })
+  }
+
+  filterABV = () => {
+    this.setState({
+      highAlcoholOnly: !this.state.highAlcoholOnly,
+    })
+  }
+
+  filterAcidic = () => {
+    this.setState({
+      highAcidityOnly: !this.state.highAcidityOnly,
+    })
+  }
+  
   render() {
-    return <h1>Let's drink beer!</h1>;
+    const {highAlcoholOnly, classicRangeOnly, highAcidityOnly, searchTerm } = this.state;
+    return (
+      <main className={styles.app}>
+        <Navbar handleSearch={this.handleSearch} filterABV={this.filterABV} filterClassic={this.filterClassic} filterAcidic={this.filterAcidic} />
+        <Main highAlcoholOnly={highAlcoholOnly} classicRangeOnly={classicRangeOnly} highAcidityOnly={highAcidityOnly} searchTerm={searchTerm} data={beers} />
+      </main>
+    )
+    ;
   }
 }
