4d19247beb2033fe54e20ce4c3db931e51be7e47
Martin Harrison <mpharrison206@gmail.com>
2020-08-05 10:41:07 +0100

    Updates

---
+++ precache-manifest.2c7725b06e047d37df14124b137bfb21.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd26deb0ce3d8c3cdaaa743e7e825a10",
+    "url": "/punk-api-app/index.html"
+  },
+  {
+    "revision": "bf598571ad6fc87e3fcc",
+    "url": "/punk-api-app/static/css/main.9d953ff0.chunk.css"
+  },
+  {
+    "revision": "2f9675ce566d1709b5f8",
+    "url": "/punk-api-app/static/js/2.e7f61295.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/punk-api-app/static/js/2.e7f61295.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf598571ad6fc87e3fcc",
+    "url": "/punk-api-app/static/js/main.c043c626.chunk.js"
+  },
+  {
+    "revision": "307ba41f3230a315eda5",
+    "url": "/punk-api-app/static/js/runtime-main.6d3e7a92.js"
+  },
+  {
+    "revision": "ef08480420d87b59cfd7552845d79328",
+    "url": "/punk-api-app/static/media/pub.ef084804.jpg"
+  }
+]);
